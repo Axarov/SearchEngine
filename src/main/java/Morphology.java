c314128037fb0248176128fe7f401608f3109a62
Fargat Axarov <axarov88@gmail.com>
2023-06-10 17:24:35 +0500

    Init

---
+++ src/main/java/Morphology.java
@@ -0,0 +1,13 @@
+import org.apache.lucene.morphology.LuceneMorphology;
+import org.apache.lucene.morphology.russian.RussianLuceneMorphology;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Morphology {
+    public static void main(String[] args) throws IOException {
+        LuceneMorphology luceneMorph = new RussianLuceneMorphology();
+        List<String> wordBaseForms = luceneMorph.getNormalForms("леса");
+        wordBaseForms.forEach(System.out::println);
+    }
+}
